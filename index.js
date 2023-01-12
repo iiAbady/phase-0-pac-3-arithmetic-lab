7167ba85ecb462e6815a8a46bfb6a84a85a5212a
Abdullah Binsaad <abdullah.binsaadd@gmail.com>
2023-01-12 21:08:18 +0300

    Completed Assignemnet.

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(n1, n2) {
+	return n1 + n2;
+}
+
+function subtract(n1, n2) {
+	return n1 - n2;
+}
+
+function multiply(n1, n2) {
+	return n1 * n2;
+}
+
+function divide(n1, n2) {
+	return n1 / n2;
+}
+
+function increment(n){
+	return ++n;
+}
+
+function decrement(n) {
+	return --n;
+}
+
+function makeInt(n) {
+	return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+	return parseFloat(n);
+}
